1e0a7dbbcd7e55fdeecc27a7aba354eeed00080a
Shivraj-Kore <shivrajkore100.com>
2023-05-02 12:59:54 -0700

    first commit

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField( max_length=50)
+
+class Message(models.Model):
+    value = models.CharField( max_length=50)
+    date = models.DateTimeField(default=datetime.now , blank=True)
+    user = models.CharField( max_length=50)
+    room = models.CharField( max_length=50)
