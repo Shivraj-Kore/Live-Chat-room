30b9e81ba7f85bad43c601b162da316504ede7d3
Shivraj-Kore <shivrajkore100.com>
2023-05-05 00:41:13 -0700

    final with html

--- base/views.py
+++ base/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render , redirect
-
+from django.http import HttpResponse , JsonResponse
 from base.models import Room , Message
+
 
 def home(request):
     return render(request , 'home.html')
 
 
 def room(request , room):
-    return render(request , 'room.html')
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name=room)
+    return render(request, 'room.html', {
+        'username': username,
+        'room': room,
+        'room_details': room_details
+    })
+
 
 
 def checkview(request):
@@ -15,9 +23,24 @@ def checkview(request):
     username = request.POST['username']
 
     if Room.objects.filter(name=room).exists():
-        return redirect ('/' + room + '/?username=' + username)
+        return redirect('/'+room+'/?username='+username)
     
     else:
         new_room = Room.objects.create(name=room)
         new_room.save()
-        return redirect('/' + room + '/?username=' + username)
+        return redirect('/'+room+'/?username='+username)
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value = message , user = username , room = room_id)
+    new_message.save()
+    return HttpResponse("Message sent successfully")
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
