95185d61806b5448d0c5bab8c2796ee7c1ee33ff
Shivraj-Kore <shivrajkore100@gmail.com>
2023-05-15 21:56:42 +0530

    Html design changes

--- base/models.py
+++ base/models.py
@@ -4,8 +4,14 @@ from datetime import datetime
 class Room(models.Model):
     name = models.CharField( max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Message(models.Model):
     value = models.CharField( max_length=5000)
     date = models.DateTimeField(default=datetime.now , blank=True)
     user = models.CharField( max_length=5000)
     room = models.CharField( max_length=5000)
+
+    def __str__(self):
+        return self.user
